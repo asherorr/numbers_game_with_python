7d919fda8ce76380a8d0029310ff4b0a0c86ef51
asherorr <asher@mailfence.com>
2022-11-18 20:34:53 -0500

    This commit will enable error catching in the start_game function.

--- wf.py
+++ wf.py
@@ -12,6 +12,7 @@ num_guesses = []
 
 random_numbers_generated = []
 
+
 def welcome_message():
     opening_message = print("""
     -----------------------------------
@@ -25,12 +26,14 @@ def welcome_message():
         print(word)
         time.sleep(.5)
 
+
 def start_game():
     random_num = random.randint(1, 10)
     while True:
         try:
             try:
-                user_selection = int(input("Pick any number from {}. ".format("1-10")))
+                user_selection = int(
+                    input("Pick any number from {}. ".format("1-10")))
                 guesses.append(user_selection)
             except ValueError:
                 raise ValueError("Please enter only a number.")
@@ -49,12 +52,21 @@ def start_game():
                 num_guesses_to_get_right.append(len(guesses))
                 print("\nYou got it! Congratulations! It took you {} guesses to get the right number.".format(len(guesses)))
                 guesses.clear()
-                #try block here
-                option_to_play_again = input("\nWould you like to play again? (Yes/No) ")
-                #if input not yes or no:
-                    #Raise ValueError:
-                #except ValueError(print message)
-                #Else:
+                input_error = True
+                while input_error:
+                    try:
+                        option_to_play_again = input("\nWould you like to play again? (Yes/No) ")
+                        valid_input_1 = "yes"
+                        valid_input_2 = "no"
+                        if valid_input_1 not in option_to_play_again.lower():
+                        #if not any(entry in option_to_play_again.lower() for entry in ("yes", "no")):
+                            raise ValueError("Oh no! We ran into an issue. Please enter only yes or no.")
+                        elif valid_input_2 not in option_to_play_again.lower():
+                            raise ValueError("Oh no! We ran into an issue. Please enter only yes or no.")
+                    except ValueError as err:
+                        print(f'{err}')
+                    else:
+                        input_error = False
                 if option_to_play_again.lower() == "yes":
                     print("Current high score: {}.".format(min(num_guesses_to_get_right)))
                     random_numbers_generated.append(random_num)
@@ -70,14 +82,16 @@ def start_game():
                 else:
                     break
 
+
 def ending_message():
     print("Thanks for playing! The program will close now.")
     sys.exit
 
-welcome_message()
+
+#welcome_message()
 start_game()
 ending_message()
 
-#Refactoring
-#try block for user errors in play again
-#fix the problem of potentially having all numbers hit that list in the case of guessing all 10 nums (clear the list after they play, perhaps.)
+#Note: Sweet! I solved the error catching problem with my option_to_play_again. Great use of the while loop!
+#Note: If no is contained within the value entered upon playing again, then the program still closes down. This would be a fantastic question for the Treehouse forums!
+#Change the valuerrror back to not in "Yes," "No" and ask the Treehouse forums!
